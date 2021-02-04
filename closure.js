22fc45a6970edda99f148bbd10b0a0adda1ed732
Sakib12345 <sakibalislam12345@gmail.com>
2021-02-05 01:07:44 +0600

    closure

--- closure.js
+++ closure.js
@@ -1,1 +1,19 @@
-closure.js
+function stopWatch() {
+    let count = 0;
+    return function() {
+        count++;
+        return count;
+    }
+}
+
+const clock1 = stopWatch()
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());
+
+const clock2 = stopWatch()
+console.log(clock2());
+console.log(clock2());
+console.log(clock2());
+console.log(clock1());
