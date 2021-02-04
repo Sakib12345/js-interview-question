a249ecd225e5aab81c7db13146339f49e097a692
Sakib12345 <sakibalislam12345@gmail.com>
2021-02-04 23:22:28 +0600

    true vs false

---
+++ truthyFalsy.js
@@ -0,0 +1,17 @@
+// 0 //false
+// const name = ''; //false
+// let name; //false
+// let name = NaN; //false
+// let name = null //false
+// false
+
+// truthy
+// '0', {}, [], ' ', true
+
+let name = 'false'
+if(name){
+    console.log('Condition is true')
+}
+else{
+    console.log('Condition is false')
+}
